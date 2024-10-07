744a1aa6ccf5d2b89bfa470d91f13238e44ebc24
richymariachristya <156060515+richymariachristya@users.noreply.github.com>
2024-10-07 14:21:03 +0530

    Final-font-update-UX

--- src/pages/home/OfferTimeline.tsx
+++ src/pages/home/OfferTimeline.tsx
@@ -10,17 +10,18 @@ interface InstitutionLogoProp {
 const OfferTimeline = ({institutionLogo,institutionName,institutionSite}:InstitutionLogoProp) => {
   return (
     <>
-      <div className='h-[40px] bg-secondary-lightest flex justify-center py-[10px]'>
+      <div className='h-[40px] bg-secondary400 flex justify-center py-[10px]'>
         <p className='text-white text-small'>This offer ends in 24h 00m 20s</p>
       </div>
       <div className='h-[40px] bg-white flex justify-center'>
-        <div className="flex flex-shrink-0 items-center">
+        <div className="flex flex-shrink-0 items-center gap-[13px]">
           <a href="https://www.idp.com/">
-          <Image src={idpLogo} alt="IDP Education" width={64} height={37} className="block h-auto w-[64px] xl:hidden ml-2" />
+          <Image src={idpLogo} alt="IDP Education" className="block h-auto w-[74px]" />
           <span className="sr-only">IDP Logo</span>
           </a>
+          <span className='border-r border-darkGrey border-solid h-[30px]'></span>
           <a href={institutionSite}>
-          <Image src={institutionLogo} alt={institutionName} width={64} height={37} className="block h-auto w-[64px] xl:hidden ml-2" />
+          <Image src={institutionLogo} alt={institutionName} width={100} height={40} className="block h-auto w-[100px]" />
           <span className="sr-only">{institutionName}</span>
           </a>
           
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     maxWidth: {
-      container: "1440px",
+      container: "1200px",
     },
     screens: {
-      xs: "350px",
+      xs: "370px",
       sm: "575px",
       md: "768px",
-      lg: "960px",
+      lg: "992px",
       xl: "1200px",
       xl2: "1300px",
       xxl: "1400px",
@@ -18,62 +18,98 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        "heading": ["Farro", "sans-serif"],
-        "para": ["Inter", "sans-serif"],
+        farro: ["Farro", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
       colors: {
-        //Text color
-        "grey": "#333333",
-        //Grey shades
-        "grey-darkest": "#333F48",
-        "grey-dark": "#666666",
-        "grey-medium": "#949494",
-        "grey-light": "#CCCCCC",
-        "grey-lighter": "#E5E5E5",
-        "grey-lightest": "#F2F2F2",
-        //primary - Blue shades
-        "primary": "#4664DC", 
-        "primary-lightest": "#EDFCFF", 
-        "primary-lighter": "#C8F0F5", 
-        "primary-light": "#0FBEF0", 
-        "primary-dark": "#28558C", 
-        "primary-petal": "#0874E7", 
-        //Secondary - green shades
-        "secondary": "#068468", 
-        "secondary-lightest": "#EEF2C0", 
-        "secondary-lighter": "#F0FAAF", 
-        "secondary-light": "#B4E646", 
-        "secondary-dark": "#1A7A75", 
-        "secondary-petal": "#46A617", 
-        // Tertiary - Orange shades
-        "tertiary": "#D18100", 
-        "tertiary-lightest": "#FFF7E0", 
-        "tertiary-lighter": "#FFF0BE", 
-        "tertiary-light": "#FFD700", 
-        "tertiary-dark": "#CB4F0A", 
-        "tertiary-petal": "#E07400", 
-        // Validation - Positive shades
-        "positive": "#168721",
-        "positive-light": "#DCEDDE",
-        "positive-dark": "#106519",
-        // Validation - Negative shades
-        "negative": "#BC0000",
-        "negative-light": "#FFBEBE",
-        "negative-dark": "#800101",
-        "ielts-red": "#E31837", // IETLS Brand FOR IELTS related pods
+         // neutral
+          neutral700: "#374151",
+          neutral500: "#6B7280",
+        // neutral
+        neutral700: "#374151",
+        neutral500: "#6B7280",
+        // grey set colors starts
+        grey50: "#F9FAFB",
+        grey100: "#F2F2F2",
+        grey200: "#D9D9D9",
+        grey300: "#ADB2B6",
+        grey400: "#82898F",
+        grey500: "#5C656E",
+        grey600: "#333F48",
+        grey700: "#767676",
+        // grey set colors ends
+        // primary (blue) set colors starts
+        primary50: "#EDF7FF",
+        primary100: "#E6F1FB",
+        primary200: "#E6F1FB",
+        primary300: "#0086F8",
+        primary400: "#4664DC",
+        primary500: "#0657AD",
+        // primary (blue) set colors ends
+        // secondary set colors starts
+        secondary100: "#FDFAAF",
+        secondary200: "#B4E646",
+        secondary300: "#51C21A",
+        secondary400: "#168721",
+        secondary500: "#1E8782",
+        // secondary set colors ends
+        // Tertiary set colors starts
+        tertiary50: "#FFF7E0",
+        tertiary100: "#FFF0BE",
+        tertiary200: "#FFD700",
+        tertiary300: "#FFA514",
+        tertiary400: "#FF8300",
+        tertiary500: "#973A14",
+        tertiary600: "#BC5B00",
+        // Tertiary set colors ends
+        // validation set colors starts
+        positive: "#168721",
+        positiveLight: "#EEF2C0",
+        positiveDark: "#106519",
+        // validation set colors ends
+        // validation set colors starts
+        negative: "#BC0000",
+        negativeLight: "#FFEEEE",
+        negativeDark: "#800101",
+        // validation set colors ends
+        // shades set colors starts
+        darkGrey: "#82898F",
+        textGrey: "#767676",
+        textBlue: "#4B4EFC",
+        // shades set colors ends
       },
       fontSize: {
-        "heading-1": ["40px", "1.3"],
-        "heading-2": ["32px", "1.3"],
-        "heading-3": ["28px", "1.3"],
-        "heading-4": ["24px", "1.3"],
-        "heading-5": ["20px", "1.3"],
-        "heading-6": ["18px", "1.3"],
-        para: ["16px", "1.5"],
-        small: ["14px", "1.4"],
-        "x-small": ["12px", "1.3"],
-      }
+        // Desktop fontsize starts
+        "heading-xl": ["5.2rem", "1.3"],
+        "heading-lg": ["4.4rem", "1.3"],
+        heading1: ["4rem", "1.3"],
+        heading2: ["3.6rem", "1.3"],
+        heading3: ["3.2rem", "1.3"],
+        heading4: ["2.8rem", "1.3"],
+        heading5: ["2.4rem", "1.3"],
+        heading6: ["2rem", "1.3"],
+        //para fontsize starts
+        "para-lg": ["1.8rem", "1.3"],
+        para: ["1.6rem", "1.5"],
+        small: ["1.4rem", "1.5"],
+        "x-small": ["1.2rem", "1.5"],
+        //para fontsize ends
+      },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(0deg, #000000 0%, rgba(0, 0, 0, 0) 60.5%, rgba(0, 0, 0, 0) 100%)",
+        "custom-gradient2":
+          "0px 4px 6px -4px #1018281A, 0px 10px 15px -3px #1018281A",
+      },
+      boxShadow: {
+        "custom-sm": "0 1px 2px 0 rgba(16, 24, 40, 0.05);",
+      },
+      boxShadow: {
+        "custom-sm": "0 1px 2px 0 rgba(16, 24, 40, 0.05);",
+        "custom-lg": "0 10px 15px -3px rgba(16, 24, 40, 0.1);",
+      },
+      
     },
   },
   plugins: [],
-};
+}
