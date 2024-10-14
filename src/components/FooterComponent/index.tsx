8cf8233382b583b31374fcedfcfcfba00aeb7efe
@hariharan.suresh <hariharan.suresh@idp.com>
2024-10-14 12:25:04 +0530

    commented env file in gitignore

--- src/components/FooterComponent/index.tsx
+++ src/components/FooterComponent/index.tsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React from "react"
 interface FooterResponse {
-  additionalResources: string;
-  contactInformation: string;
+    additionalResources: string
+    contactInformation: string
 }
 interface FooterProps {
-  footerResponse: FooterResponse;
+    footerResponse: FooterResponse
 }
- const FooterComponent = ({ footerResponse }:FooterProps) =>{
-  return (
-    <footer className="bg-neutral-50 container rounded-lg mx-auto"
-    >
-      <div className="flex flex-col items-center justify-between p-4">
-        <p className="text-grey-300 small font-inter font-normal">
-           Don’t want to receive any offers in future? 
-           <a className="underline text-primary-400 hover:no-underline cursor-pointer"> Stop sending Proposals</a>
-        </p>
-      </div>
-    </footer>
-  );
-};
+const FooterComponent = ({ footerResponse }: FooterProps) => {
+    return (
+        <footer className="bg-neutral-50 container rounded-lg mx-auto">
+            <div className="flex flex-col items-center justify-between p-4">
+                <p className="text-grey-300 small font-inter font-normal">
+                    Don’t want to receive any offers in future?
+                    <a className="underline text-primary-400 hover:no-underline cursor-pointer">
+                        {" "}
+                        Stop sending Proposals
+                    </a>
+                </p>
+            </div>
+        </footer>
+    )
+}
 export default FooterComponent
